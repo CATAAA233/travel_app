65c145d27ca3d3460f26d16f36077de9f550c396
Carlos Catalan <carlosdanielcatalan1o@gmail.com>
2024-06-26 15:08:35 -0700

    Init Proyect

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/config/theme/app_theme.dart';
+import 'package:travel_app/presentation/screens/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Travel App',
+      theme: AppTheme().getTheme(),
+      routes: {
+        '/':(context) => const WelcomeScreen()
+      },
+    );
+  }
+}
