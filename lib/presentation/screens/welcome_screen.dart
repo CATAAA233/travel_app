32dd8ca6012ac7dbac22fa7d609697c8c74373b9
Carlos Catalan <carlosdanielcatalan1o@gmail.com>
2024-07-03 21:06:20 -0700

    Feat(Welcome Screen): Creating Basic structure of screen

--- lib/presentation/screens/welcome_screen.dart
+++ lib/presentation/screens/welcome_screen.dart
@@ -5,10 +5,52 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('data hello'),
-      ),
-    );
+    return Scaffold(
+        body: Stack(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 120, horizontal: 0),
+            child: Column(
+              children: [
+                const Placeholder(
+                  fallbackHeight: 70,
+                ),
+                const Expanded(child: Placeholder()),
+                Column(
+                  children: [
+                    const Text(
+                      'Discover\nNew trails',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 40,
+                        fontWeight: FontWeight.w700,
+                        height: 1.1,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 40),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 13, horizontal: 30),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.red),
+                      child: const Text(
+                        'Get Started',
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
