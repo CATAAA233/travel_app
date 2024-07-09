541d273ea0a7e51356ac25efb4d557d4e14a272f
Carlos Catalan <carlosdanielcatalan1o@gmail.com>
2024-07-08 19:36:11 -0700

    Feat(Welcome screen): ScrollsContainer Created

--- lib/presentation/screens/welcome_screen.dart
+++ lib/presentation/screens/welcome_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -11,20 +8,17 @@ class WelcomeScreen extends StatelessWidget {
     return const Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10 , vertical: 0),
-          child: Stack(
-            children:[
-              Header(),
-              ScrollsContainer(),
-              FoteerSection()
-            ]
-          ),
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+          child: Stack(children: [
+            ScrollsContainer(),
+            Header(),
+            FoteerSection(),
+          ]),
         ),
       ),
     );
   }
 }
-
 
 class Header extends StatelessWidget {
   const Header({
@@ -34,34 +28,22 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment:  Alignment.topCenter,
+      alignment: Alignment.topCenter,
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
           Expanded(
             child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.red
-                )
-              ),
+              decoration: BoxDecoration(border: Border.all(color: Colors.red)),
               height: 100,
-              child:const  Center(
-                child: Text('Logo 2')
-              ),
+              child: const Center(child: Text('Logo 2')),
             ),
           ),
           Expanded(
             child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.red
-                )
-              ),
+              decoration: BoxDecoration(border: Border.all(color: Colors.red)),
               height: 100,
-              child:const  Center(
-                child: Text('Logo 2')
-              ),
+              child: const Center(child: Text('Logo 2')),
             ),
           ),
         ],
@@ -77,8 +59,61 @@ class ScrollsContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Scrolls Body'),
+    return const Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          CustomListView(),
+          SizedBox(width: 20),
+          CustomListView(),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomListView extends StatelessWidget {
+  const CustomListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemBuilder: (context, _) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: ImageCard(),
+          );
+        },
+        itemCount: 5,
+      ),
+    );
+  }
+}
+
+class ImageCard extends StatelessWidget {
+  const ImageCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 9 / 16,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            'assets/images/landscapes/pexels-packermann-1666021.jpg',
+            fit: BoxFit.cover,
+          ),
+        )
+      ),
     );
   }
 }
@@ -100,11 +135,10 @@ class FoteerSection extends StatelessWidget {
             Text(
               'Discover\nNew Trails',
               style: TextStyle(
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w700,
-                fontSize: 28,
-                height: 1.2
-              ),
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 28,
+                  height: 1.2),
               textAlign: TextAlign.center,
             ),
             SizedBox(height: 25),
@@ -124,16 +158,12 @@ class CustomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
-      },
+      onTap: () {},
       child: Container(
         decoration: const BoxDecoration(
-          borderRadius: BorderRadius.horizontal(
-            left: Radius.circular(100),
-            right: Radius.circular(100)
-          ),
-          color: Colors.orange
-        ),
+            borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(100), right: Radius.circular(100)),
+            color: Colors.orange),
         child: const Padding(
           padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: Text('Get Started'),
